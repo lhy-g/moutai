4f659a12865753e12bb18465ef44bdfa98c63359
caiyong520 <1067655314@qq.com>
2020-09-28 10:55:13 +0800

    111

--- src/main/java/com/tongchuang/general/GeneralApplication.java
+++ src/main/java/com/tongchuang/general/GeneralApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GeneralApplication {
     public static void main(String[] args) {
         SpringApplication.run(GeneralApplication.class, args);
-        System.out.println("GeneralApplication 启动成功!!!!!!!!!!!");
+        System.out.println("GeneralApplication 启动成功!!");
     }
 
 }
